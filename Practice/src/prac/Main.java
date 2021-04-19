381479bcac3e4bad6353c279947507fc887efa89
unknown <Sagargoyal692001@gmail.com>
2021-04-19 13:15:21 +0530

    third commit

--- Practice/src/prac/Main.java
+++ Practice/src/prac/Main.java
@@ -4,6 +4,9 @@ public class Main {
 	public static void main(String[] args){
 		System.out.println("Hello ! World!!");
 		System.out.println("Sagar Goyal");
+		System.out.println("Jatin Goyal");
+		System.out.println("Vishal Goyal");
+		System.out.println("Satvik Goyal");
 		
 	}
 
